1842079a4359bc82e980c28bfc1206ec3b2d9517
Woo Song <ws392@drexel.edu>
2018-07-27 15:53:13 -0400

    WS: Update solution for challenge 4

--- challenge_4/ws/README.md
+++ challenge_4/ws/README.md
@@ -2,8 +2,9 @@
 
 ## How to Run:
 1. Run `npm install` in the root of project folder
-2. Run `gulp` to run default tasks: `styles` and `html`
+2. Run `gulp` to run default tasks: `styles`, `html`, and `watch`
 3. Output files will be created in `dist/`
+4. Browser will automatically open `dist/index.html`
 
 
 ## Note: 
--- challenge_4/ws/gulpfile.js
+++ challenge_4/ws/gulpfile.js
@@ -1,25 +1,37 @@
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const stylus = require('gulp-stylus');
-const refresh = require('gulp-refresh');
+const browserSync = require('browser-sync');
+const reload = browserSync.reload;
 
-gulp.task('default', ['styles', 'html']);
+// default task that will run all available tasks
+gulp.task('default', ['styles', 'html', 'watch']);
 
+// task that will compile stylus file and refresh browser
 gulp.task('styles', () => {
     return gulp.src('./src/stylus/screen.styl')
-        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init())    
         .pipe(stylus())
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist/css/'))
-        .pipe(refresh());
+        .pipe(reload({ stream:true }));
 });
 
+// task that will copy html to dist/
 gulp.task('html', () => {
     return gulp.src('./src/views/index.html')
         .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('watch', () => {
-    refresh.listen();
-	gulp.watch('./src/stylus/screen.styl', ['styles']);
+// task that will watch for changes made to stylus
+gulp.task('watch', ['styles','html'], function() {
+
+    browserSync({
+        server: {
+          baseDir: 'dist'
+        }
+      });
+    
+
+    gulp.watch('src/stylus/screen.styl', ['styles']);
 });
